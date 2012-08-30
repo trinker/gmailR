a1c3357795ab3705929c3ce5503ac5d7ca74dca7
tyler rinker <tyler_rinker@hotmail.com>
2012-08-30 00:31:37 -0400

    added onload of data and a root change ability for username

--- R/email.R
+++ R/email.R
@@ -1,7 +1,21 @@
 email <-
-function(to, password, subject="R message", message="EOM", attachment=NULL,  
-    server="smtp.gmail.com:587", from=FROM, username=UNAME, 
-    confirmBeforeSend=FALSE){
+function(to, password, subject="R message", message="EOM", from=NULL, 
+    attachment=NULL, server="smtp.gmail.com:587", username=UNAME, 
+    confirmBeforeSend=FALSE, clear.username = FALSE){
+    data(UNAME)
+    loc <- paste0(find.package("gmailR"), "/data")
+    if ((is.null(username) & is.na(UNAME) & !exists(".UNAME", 
+        envir = .GlobalEnv)) | clear.username) {
+        cat("\n","Enter gmail Username","\n")
+        UNAME <- scan(n=1,what = character(0), quiet=T)
+        .UNAME <<- UNAME 
+        unlink(paste0(loc, "/UNAME.rda"), recursive = TRUE, 
+            force = FALSE)
+        save(UNAME, file = paste0(loc, "/UNAME.rda"))
+    }     
+    if (is.null(from)) {
+        from <- username
+    } 
     lapply(to,
         function (x){
             email.helper(to=list(x), 
---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onLoad <- function(libname = find.package("gmailR"), 
+    pkgname = "gmailR") {
+    data(cell.ext, package = "gmailR")
+}
