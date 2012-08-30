f800f71d3b73d5771da702c11073e29e5a4ea060
tyler rinker <tyler_rinker@hotmail.com>
2012-08-30 00:16:13 -0400

    added formatting

--- R/email.R
+++ R/email.R
@@ -3,16 +3,16 @@ function(to, password, subject="R message", message="EOM", attachment=NULL,
     server="smtp.gmail.com:587", from=FROM, username=UNAME, 
     confirmBeforeSend=FALSE){
     lapply(to,
-    function (x){
-        email.helper(to=list(x), 
-            from = list(from),
-            subject = subject,
-            message = message, 
-            attachment = attachment,
-            username = username, 
-            password = password, 
-            server = server, 
-            confirmBeforeSend = confirmBeforeSend)
-           }
+        function (x){
+            email.helper(to=list(x), 
+                from = list(from),
+                subject = subject,
+                message = message, 
+                attachment = attachment,
+                username = username, 
+                password = password, 
+                server = server, 
+                confirmBeforeSend = confirmBeforeSend)
+        }
     )
 }
